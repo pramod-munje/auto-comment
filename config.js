5095f6e8a3c27496d877118de9c91a3f969fc258
pramod-munje <pramod-munje@users.noreply.github.com>
2026-07-06 14:41:50 +0000

    chore(version): bump patch version

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 const CONFIG = {
     // Version
-    version: "1.0.3",
+    version: "1.0.4",
 
     // Canvas settings
     canvasWidth: 400,
@@ -42,7 +42,7 @@ const CONFIG = {
     pauseButtonText: "Pause",
     resumeButtonText: "Resume",
     instructionsText: "Use arrow keys or WASD to move",
-    footerText: "Built with ❤️ | Version 1.0.3",
+    footerText: "Built with ❤️ | Version 1.0.4",
 
     // [CONFIG_MARKER]
 };
