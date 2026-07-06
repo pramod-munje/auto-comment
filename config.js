bb9ff1101f7da27878ba825d23569cd4b962e131
pramod-munje <pramod-munje@users.noreply.github.com>
2026-07-06 21:08:18 +0000

    style(canvas): adjust background shade

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ const CONFIG = {
     snakeColor: "#10b981",
     snakeHeadColor: "#00ffcc",
     foodColor: "#fb923c",
-    backgroundColor: "#111118",
+    backgroundColor: "#0c0c14",
     gridColor: "#15151f",
 
     // UI text
