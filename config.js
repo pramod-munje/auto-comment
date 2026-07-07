7fba2e5f2cc19b665fa478528c17ebb28d40844a
pramod-munje <pramod-munje@users.noreply.github.com>
2026-07-07 22:46:14 +0000

    feat(scoring): update score multiplier

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ const CONFIG = {
     // Scoring
     pointsPerFood: 20,
     levelUpScore: 60,
-    scoreMultiplier: 1.1,
+    scoreMultiplier: 1.5,
 
     // Game behavior
     wallCollision: true,
