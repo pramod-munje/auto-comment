019104555d36080577a4fe6980326f5ba48ff625
pramod-munje <pramod-munje@users.noreply.github.com>
2026-07-11 06:32:12 +0000

    style(snake): refine head color

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ const CONFIG = {
 
     // Visual settings
     snakeColor: "#4ade80",
-    snakeHeadColor: "#00ffcc",
+    snakeHeadColor: "#a7f3d0",
     foodColor: "#f97316",
     backgroundColor: "#0c0c14",
     gridColor: "#15151f",
