640e583b5114c712f320edf2dcf35e7203f14968
pramod-munje <pramod-munje@users.noreply.github.com>
2026-08-09 11:31:15 +0000

    chore(version): bump patch version

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 
 const CONFIG = {
     // Version
-    version: "1.0.233",
+    version: "1.0.234",
 
     // Canvas settings
     canvasWidth: 400,
@@ -42,7 +42,7 @@ const CONFIG = {
     pauseButtonText: "Pause",
     resumeButtonText: "Resume",
     instructionsText: "Use arrow keys or WASD to move",
-    footerText: "Built with ❤️ | Version 1.0.233",
+    footerText: "Built with ❤️ | Version 1.0.234",
 
     // Food glow effect
     foodGlow: true,
