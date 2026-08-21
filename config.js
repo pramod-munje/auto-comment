bd020b9029f0718fbcaca4f75cf3d72ae90cdea2
pramod-munje <pramod-munje@users.noreply.github.com>
2026-08-21 22:35:31 +0000

    style(food): update food color for visibility

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ const CONFIG = {
     // Visual settings
     snakeColor: "#059669",
     snakeHeadColor: "#00ffcc",
-    foodColor: "#eab308",
+    foodColor: "#fbbf24",
     backgroundColor: "#111118",
     gridColor: "#15151f",
 
