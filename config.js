c5930c1ef5a01de744311ac17f5ae4b9b6f69784
pramod-munje <pramod-munje@users.noreply.github.com>
2026-08-23 19:48:48 +0000

    style(snake): update snake body color

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ const CONFIG = {
     showParticles: false,
 
     // Visual settings
-    snakeColor: "#00d4aa",
+    snakeColor: "#4ade80",
     snakeHeadColor: "#bbf7d0",
     foodColor: "#f43f5e",
     backgroundColor: "#0c0c14",
