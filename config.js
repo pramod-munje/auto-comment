65437d434ad839934786eb9a48861d12a85eb44c
pramod-munje <pramod-munje@users.noreply.github.com>
2026-08-25 21:58:50 +0000

    ui(header): refresh subtitle text

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ const CONFIG = {
 
     // UI text
     gameTitle: "🐍 Snake Challenge",
-    gameSubtitle: "The ultimate snake challenge",
+    gameSubtitle: "How long can you last?",
     gameOverText: "You Crashed!",
     startButtonText: "Start Game",
     pauseButtonText: "Pause",
