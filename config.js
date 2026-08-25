d6c7e0d8d7017ff98eef3809b3505d85434eeca1
pramod-munje <pramod-munje@users.noreply.github.com>
2026-08-25 07:16:00 +0000

    ui(gameover): update game over text

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ const CONFIG = {
     // UI text
     gameTitle: "🐍 Snake Master",
     gameSubtitle: "Eat, grow, survive",
-    gameOverText: "Better Luck Next Time!",
+    gameOverText: "Try Again!",
     startButtonText: "Start Game",
     pauseButtonText: "Pause",
     resumeButtonText: "Resume",
