169d318919a9fd9193a7a3cf693db7866a6c8a89
pramod-munje <pramod-munje@users.noreply.github.com>
2026-09-20 19:22:22 +0000

    ui(header): update game title

--- config.js
+++ config.js
@@ -35,7 +35,7 @@ const CONFIG = {
     gridColor: "#15151f",
 
     // UI text
-    gameTitle: "🐍 Snake Challenge",
+    gameTitle: "🐍 Neon Snake",
     gameSubtitle: "Classic arcade reborn",
     gameOverText: "You Crashed!",
     startButtonText: "Start Game",
